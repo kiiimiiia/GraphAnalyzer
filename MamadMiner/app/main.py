839f3681e55dda996fa1db1b66ff0e7ab51e59fe
“iMamad <seyedmohammad.farrahi@stud-mail.uni-wuerzburg.de>
2023-06-19 16:37:06 +0200

    main.py file(controller) has been implemented.

---
+++ MamadMiner/app/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+from collections import defaultdict
+import git2net
+import os
+import sqlite3
+import pandas as pd
+import networkx as nx
+from app.analyser import get_graph
+from app.cloner import clone_and_mine
+app = Flask(__name__)
+
+node_info = []
+edge_info = []
+
+@app.route('/mine_repo', methods=['POST'])
+def mine_repo():
+
+    repo_url = request.json.get('url') # Get URL from the POST request body
+    
+    sqlite_db_file = clone_and_mine(repo_url)
+    
+    n, node_info, edge_info = get_graph(sqlite_db_file)
+    node_info = node_info
+    edge_info = edge_info
+
+    return jsonify({"message": "Repo mined successfully"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
