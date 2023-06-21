4bfb7d3ea811a91d43b5f98c78f02cfd1a4f44ec
“iMamad <seyedmohammad.farrahi@stud-mail.uni-wuerzburg.de>
2023-06-21 07:57:41 +0200

    added analyser file a function to analyse based on date.

--- MamadMiner/app/analyser.py
+++ MamadMiner/app/analyser.py
@@ -2,6 +2,7 @@
 
 import git2net
 import pathpy as pp
+from datetime import datetime
 
 def make_coediting_net(db_filename):
     t, node_info, edge_info = git2net.get_bipartite_network(db_filename)
@@ -13,4 +14,9 @@ def get_graph(sqlite_db_file):
     n, node_info, edge_info = make_coediting_net(sqlite_db_file)
     return n, node_info, edge_info
     
-    
+
+def get_graph(sqlite_db_file, from_date):
+    time_from = datetime(2019, 5, 1)
+    t, node_info, edge_info = git2net.get_bipartite_network(sqlite_db_file, time_from=from_date)
+    n = pp.Network.from_temporal_network(t)
+    return n, node_info, edge_info
