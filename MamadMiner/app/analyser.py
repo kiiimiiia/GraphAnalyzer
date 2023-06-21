6fe1dd320006e15ebf242cd3d59ed34a43f80958
“iMamad <seyedmohammad.farrahi@stud-mail.uni-wuerzburg.de>
2023-06-21 09:19:30 +0200

    commited all changes. gitfix

--- MamadMiner/app/analyser.py
+++ MamadMiner/app/analyser.py
@@ -10,13 +10,13 @@ def make_coediting_net(db_filename):
     return (n, node_info, edge_info)
 
 
-def get_graph(sqlite_db_file, from_date):
+def get_graph(sqlite_db_file):
+    t, node_info, edge_info = git2net.get_bipartite_network(sqlite_db_file)
+    n = pp.Network.from_temporal_network(t)
+    return n, node_info, edge_info
 
-    if from_date == None:
-        n, node_info, edge_info = make_coediting_net(sqlite_db_file)
-        return n, node_info, edge_info
-    else:
-        t, node_info, edge_info = git2net.get_bipartite_network(sqlite_db_file, from_date)
-        n = pp.Network.from_temporal_network(t)
-        return n, node_info, edge_info
+def get_graph(sqlite_db_file, from_date):
     
+    t, node_info, edge_info = git2net.get_bipartite_network(sqlite_db_file, time_from= from_date)
+    n = pp.Network.from_temporal_network(t)
+    return n, node_info, edge_info
